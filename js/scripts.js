64a0af5d67b9a42c4f9efd271e71897850ee2ae2
p17195197@my365.dmu.ac.uk <p17195197@my365.dmu.ac.uk>
2020-04-19 18:13:52 +0530

    menu handling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-console.log("hello");
+document.getElementById("hamburger-menu").addEventListener("click", (el) => {
+  let element = document.getElementById("full-menu");
+  element.classList.contains("hidden")
+    ? element.classList.remove("hidden")
+    : element.classList.add("hidden");
+});
+
+document.getElementById("close-button").addEventListener("click", (el) => {
+  document.getElementById("full-menu").classList.add("hidden");
+});
+
+document
+  .getElementById("full-menu")
+  .querySelectorAll("li")
+  .forEach((el) => {
+    el.addEventListener("click", function () {
+      document.getElementById("full-menu").classList.add("hidden");
+    });
+  });
