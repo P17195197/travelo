c68d8794062bd196a6e755f0e349500aa58e582c
p17195197@my365.dmu.ac.uk <p17195197@my365.dmu.ac.uk>
2020-04-20 15:39:23 +0530

    newsletter subscription

---
+++ js/menu.js
@@ -0,0 +1,45 @@
+document.getElementById("hamburger-menu").addEventListener("click", (el) => {
+  let element = document.getElementById("full-menu");
+  element.classList.contains("hidden")
+    ? element.classList.remove("hidden")
+    : element.classList.add("hidden");
+});
+
+document.getElementById("close-button").addEventListener("click", (el) => {
+  document.getElementById("full-menu").classList.add("hidden");
+});
+
+document
+  .getElementById("full-menu")
+  .querySelectorAll("li")
+  .forEach((el) => {
+    el.addEventListener("click", function () {
+      document.getElementById("full-menu").classList.add("hidden");
+      if (!document.getElementById("header").classList.contains("move-up")) {
+        document.getElementById("header").classList.add("move-up");
+      }
+      if (this === this.parentElement.querySelector("li")) {
+        document.getElementById("header").classList.remove("move-up");
+      }
+    });
+  });
+
+document
+  .getElementById("main-menu")
+  .querySelectorAll("li")
+  .forEach((el) => {
+    el.addEventListener("click", function () {
+      this.parentElement.querySelectorAll("li").forEach((li) => {
+        if (li.classList.contains("active")) {
+          li.classList.remove("active");
+        }
+      });
+      if (!document.getElementById("header").classList.contains("move-up")) {
+        document.getElementById("header").classList.add("move-up");
+      }
+      if (this === this.parentElement.querySelector("li")) {
+        document.getElementById("header").classList.remove("move-up");
+      }
+      this.classList.add("active");
+    });
+  });
